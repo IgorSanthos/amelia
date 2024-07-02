e805b89dfe73e986d1f9abff3a0a9fd9dcd0c185
igorsanthos <igorsanthoss@gmail.com>
2024-07-02 13:56:16 -0300

    second commit

--- .vscode/position.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-print("Mova o mouse para a posição desejada e aguarde...")
-time.sleep(8)
-currentMouseX, currentMouseY = pyautogui.position()
-print(f"x={currentMouseX} y={currentMouseY}pa.click(x, y)")
-
-
-
