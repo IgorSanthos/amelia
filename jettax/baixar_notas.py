7b9642bcdb0eb96a1f916060eb93d8f20f224455
igorsanthos <igorsanthoss@gmail.com>
2024-07-04 07:45:36 -0300

    commit 04/07/2024

---
+++ jettax/baixar_notas.py
@@ -0,0 +1,30 @@
+import pyautogui as pa
+import time
+import os
+
+# abrir google
+# https://app.jettax.com.br/login
+time.sleep(1)
+os.system('start chrome')
+time.sleep(2)
+pa.write('https://app.jettax.com.br/login')
+pa.press('enter')    # Pressiona Enter para abrir o Chrome
+time.sleep(5)
+# se janela open= APP - Jettax - Automação Fiscal - Google Chrome
+    # login jettax
+    # suellen@fenixconsultores.com.br
+    # 123456
+#else Jettax - Automação Fiscal - Google Chrome
+
+# entrar
+
+# ctrl+f
+
+# Jettax Federal
+# NFe
+# Donwload
+# solicitação
+# selecionar os clientes
+# Peridodo Inicial
+# Periodo Final
+# Solicitar
